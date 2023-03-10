5ded0a5fe0e9efb8db326192fdf0e8d9870f2531
thitiphat.ko <thitiphat.ko@gmail.com>
2023-03-10 15:24:17 +0700

    Data types

--- Day 2 - Understanding Data Types and How to Manipulate Strings/Data types.py
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Data types.py
@@ -1,6 +1,29 @@
 # Data types
 
-# String
+# String - a piece of text inside "" even numbers
 # always start counting from 0
 
 print('Hello'[0])
+
+# the last character starts from -1
+
+print('Hello'[-1])
+
+#inside "" below considered as strings
+
+print("123" + "456")
+
+# Integer
+
+print(123 + 456)
+
+# use _ as , to split large number such as 123_456_789 = 123,456,789
+
+# Float = number with decimal places
+
+3.14
+
+# Boolean = 2 possible values True or False
+
+True
+False
